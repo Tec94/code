d474dbbddbeb7c6812abdbad26064dc0bd8fa8a8
Tec94 <caodangkhiet1@gmail.com>
2022-10-15 22:13:34 -0500

    add a piece_logic file that stores the direction for each piece during the game

---
+++ chess/piece_logic.js
@@ -0,0 +1,14 @@
+var horizontal = false;
+var vertical = false;
+var diagonal = false;
+var LShape = false;
+var distance = 0;
+piece_logic = {
+    'b_rook.png':{
+        horizontal : true,
+        vertical : true,
+        diagonal : false,
+        LShape : false,
+        distance : 7
+    }
+}
