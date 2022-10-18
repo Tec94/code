25d4f071ffa8ecddbb25551d7bc6ce391a0f3160
Tec94 <caodangkhiet1@gmail.com>
2022-10-17 22:00:50 -0500

    add logic attributes for each piece in piece_logic

--- chess/piece_logic.js
+++ chess/piece_logic.js
@@ -4,11 +4,33 @@ var diagonal = false;
 var LShape = false;
 var distance = 0;
 piece_logic = {
-    'b_rook.png':{
-        horizontal : true,
-        vertical : true,
-        diagonal : false,
-        LShape : false,
-        distance : 7
+    'rook.png':{
+        horizontal: true,
+        vertical: true,
+        distance: 7
+    },
+    'knight.png':{
+        LShape: true,
+        distance: 2
+    },
+    'bishop.png':{
+        diagonal: true,
+        distance: 7
+    },
+    'pawn.png':{
+        vertical: true,
+        distance: 2
+    },
+    'king.png':{
+        horizontal: true,
+        vertical: true,
+        diagonal: true,
+        distance: 1
+    },
+    'queen.png':{
+        horizontal: true,
+        vertical: true,
+        diagonal: true,
+        distance: 7
     }
 }
