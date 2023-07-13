1dcd7a859fc9aac7abfe5ed378b9be00d6e9f94a
Tec94 <caodangkhiet1@gmail.com>
2023-07-13 15:34:30 +0700

    rename folder to python_jizz

--- python_jizz/ObjectDetection/ObjectDetection.py
+++ python_jizz/ObjectDetection/ObjectDetection.py
@@ -1,10 +1,13 @@
 from imageai.Detection import ObjectDetection
 import os
 
+# change dir to resource folder in ObjectDetection folder
+os.chdir(r"C:\Users\user\Desktop\New folder\code\python jizz\ObjectDetection\resources")
 execution_path = os.getcwd()
+print(execution_path)
 detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
-detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
 detector.loadModel()
 detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
 for eachObject in detections:
