894c80aca0fea2c0e27a4cb3c1c3d270255fd07d
Tec94 <caodangkhiet1@gmail.com>
2023-07-17 04:27:46 -0500

    add some test statements in sql db

--- sql/test.session.sql
+++ sql/test.session.sql
@@ -17,3 +17,21 @@ VALUES
 
 -- @block
 SELECT * FROM Users;
+
+
+-- @block
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    phone_number VARCHAR(20),
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
